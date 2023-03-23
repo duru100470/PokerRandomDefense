072db62403756bdbaa5056946b2151b46a426372
duru100470 <zenpia@naver.com>
2023-03-23 14:05:09 +0900

    Player W.I.P

--- Assets/Scripts/GamePlay/Player.cs
+++ Assets/Scripts/GamePlay/Player.cs
@@ -1,9 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using PokerRandomDefense.GamePlay.Stats;
 using UnityEngine;
 using VContainer;
 
-public class Player : MonoBehaviour
+namespace PokerRandomDefense.GamePlay
 {
+    public class Player : MonoBehaviour
+    {
+        [Inject]
+        private readonly GameStats _gameStats;
+        [Inject]
+        private readonly Market _market;
+        private Card[] cardArray;
+        private Tower[] towerArray;
+        [SerializeField]
+        private int handCount = 7;
 
+        public Card[] CardArray => cardArray;
+
+        private void Awake()
+        {
+            cardArray = new Card[handCount];
+            towerArray = Enumerable.Repeat<Tower>(new Tower(), 4).ToArray<Tower>();
+        }
+
+        public void SellCard(int index)
+        {
+            _gameStats.Gold += cardArray[index].Price / 2;
+            cardArray[index] = null;
+        }
+
+        public void InsertCard(int towerIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetDamage(int amount)
+        {
+            _gameStats.Health -= amount;
+        }
+    }
 }
